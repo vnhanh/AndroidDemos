37a51553332e0648cdf96cd183cdb4887b2babf5
vnhanh <nghiamy15@gmail.com>
2023-06-10 23:41:11 +0700

    feat(compose): add more dependencies

--- app/src/main/java/com/alanvo/androiddemo/features/compose/FirstComposeActivity.kt
+++ app/src/main/java/com/alanvo/androiddemo/features/compose/FirstComposeActivity.kt
@@ -3,13 +3,20 @@ package com.alanvo.androiddemo.features.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.runtime.Composable
+import androidx.compose.material3.Text
 
 
 class FirstComposeActivity: ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent { // must add dependency activity-compose
-
+            MessageCard("Alan")
         }
+    }
+
+    @Composable
+    fun MessageCard(name: String) {
+        Text(text = "Hello $name")
     }
 }

16f311e48740c1256c291c0eed76e79f916120e8
vnhanh <nghiamy15@gmail.com>
2023-06-13 22:27:32 +0700

    feat(security): practice jectpack compose

--- security/src/main/java/com/alanvo/androiddemo/security/SecurityActivity.kt
+++ security/src/main/java/com/alanvo/androiddemo/security/SecurityActivity.kt
@@ -3,44 +3,73 @@ package com.alanvo.androiddemo.security
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.BoxScopeInstance.align
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.alanvo.androiddemo.security.ui.theme.AndroidDemoTheme
 
 class SecurityActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            AndroidDemoTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            GreetingPreview()
         }
     }
 }
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Column(
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier.padding(8.dp),
+    ) {
+        Text(
+            text = "Hello $name!",
+            fontSize = 32.sp,
+            lineHeight = 36.sp,
+            modifier = modifier,
+        )
+        Text(
+            text = "From Emma",
+            fontSize = 24.sp,
+            lineHeight = 28.sp,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .padding(16.dp)
+        )
+        Image(
+            painter = painterResource(id = R.drawable.androidparty),
+            contentDescription = "Birthday",
+            modifier = Modifier.padding(8.dp).align(Alignment.CenterHorizontally)
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     AndroidDemoTheme {
-        Greeting("Android")
+        // A surface container using the 'background' color from the theme
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            Greeting("Android")
+        }
     }
 }
